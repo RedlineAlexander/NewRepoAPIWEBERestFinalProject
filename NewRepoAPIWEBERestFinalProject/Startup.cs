915a7f8e20751d492ae97b0540d793d24c867deb
RedlineAlexander <redlinerailmetro@gmail.com>
2020-07-26 21:36:07 +0300

    FinalProjectConnections

--- NewRepoAPIWEBERestFinalProject/Startup.cs
+++ NewRepoAPIWEBERestFinalProject/Startup.cs
@@ -5,24 +5,49 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore.SqlServer;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using NewRepoAPIWEBERestFinalProject.Models;
+using NewRepoAPIWEBERestFinalProject.Models.Repositories;
+using Microsoft.EntityFrameworkCore;
 
 namespace NewRepoAPIWEBERestFinalProject
 {
     public class Startup
     {
+        private IConfiguration _configuration { get; set; }
         public Startup(IConfiguration configuration)
         {
-            Configuration = configuration;
+            _configuration = configuration;
         }
 
-        public IConfiguration Configuration { get; }
+       
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            //Services
+            services.AddScoped<IPatientsRepository, SqlPatientsRepository>();
+            services.AddScoped<IServicesRepository, SqlServicesRepository>();
+            services.AddScoped<IOrdersRepository, SqlOrdersRepository>();
+
+            //DbContext
+            services.AddDbContext<NewRepoAPIWEBRestFinalProjectContext>(builder => builder.UseSqlServer(_configuration.GetConnectionString("NewRepoAPIWEBRestFinalProjectDbConnectionNew")).UseLazyLoadingProxies());
+
+            //Identity
+            services.AddIdentity<IdentityUser, IdentityRole>().AddEntityFrameworkStores<NewRepoAPIWEBRestFinalProjectContext>();
+            services.Configure<IdentityOptions>(options =>
+            {
+                options.Password.RequireDigit = false;
+                options.Password.RequireLowercase = true;
+                options.Password.RequireNonAlphanumeric = false;
+                options.Password.RequireUppercase = false;
+                options.Password.RequiredLength = 3;
+                options.Password.RequiredUniqueChars = 1;
+            });
             services.AddControllersWithViews();
         }
 
@@ -44,6 +69,7 @@ namespace NewRepoAPIWEBERestFinalProject
 
             app.UseRouting();
 
+            app.UseAuthentication();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
