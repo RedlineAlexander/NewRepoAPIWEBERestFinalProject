f469eaf424ec98b926aba0d96dc432bb78952f23
RedlineAlexander <redlinerailmetro@gmail.com>
2020-07-26 21:53:08 +0300

    Controllers

--- NewRepoAPIWEBERestFinalProject/Controllers/OrdersControllers.cs
+++ NewRepoAPIWEBERestFinalProject/Controllers/OrdersControllers.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using NewRepoAPIWEBERestFinalProject.Models;
 using NewRepoAPIWEBERestFinalProject.Models.Repositories;
 using System;
 using System.Collections.Generic;
@@ -14,6 +16,28 @@ namespace NewRepoAPIWEBERestFinalProject.Controllers
         public OrdersControllers(IOrdersRepository repository)
         {
             _repository = repository;
+        }
+
+        [AllowAnonymous]
+        public IActionResult Index(List<int> ordersIds)
+        {
+            var patients = ordersIds?.Any() != true ? _repository.GetAllOrders() : _repository.GetAllOrders().Where(x => ordersIds.Contains(x.BookingId));
+            return View(patients.ToList());
+
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        public IActionResult Create(Orders orders)
+        {
+            _repository.CreateOrder(orders);
+            return View();
         }
 
 
--- NewRepoAPIWEBERestFinalProject/Controllers/ServicesControllers.cs
+++ NewRepoAPIWEBERestFinalProject/Controllers/ServicesControllers.cs
@@ -1,12 +1,45 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using NewRepoAPIWEBERestFinalProject.Models.Repositories;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using NewRepoAPIWEBERestFinalProject.Models;
 
 namespace NewRepoAPIWEBERestFinalProject.Controllers
 {
     public class ServicesControllers : Controller
     {
+
+        private IServicesRepository _repository { get; set; }
+
+
+        public ServicesControllers(IServicesRepository repository)
+        {
+            _repository = repository;
+        }
+        [AllowAnonymous]
+        public IActionResult Index(List<int> servicesIds)
+        {
+            var services = servicesIds?.Any() != true ? _repository.GetAllServices() : _repository.GetAllServices().Where(x => servicesIds.Contains(x.ServiceID));
+            return View(services.ToList());
+
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        public IActionResult Create(Services services)
+        {
+            _repository.CreateService(services);
+            return View();
+        }
+
     }
 }
